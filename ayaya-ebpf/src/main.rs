48e154a0d8700ad364f0011f63f583f75f4766e9
menixator <3684951+menixator@users.noreply.github.com>
2025-01-20 12:50:04 +0500

    feat: filter file_open events with a static path

--- ayaya-ebpf/src/main.rs
+++ ayaya-ebpf/src/main.rs
@@ -22,8 +22,37 @@ pub fn file_open(ctx: LsmContext) -> i32 {
     }
 }
 
+pub fn get_path_from_file(file: *const vmlinux::file, buf: *const u8, size: u32) -> i64 {
+    // Get a pointer to the file path
+    let path = unsafe { &((*file).f_path) as *const _ as *mut aya_ebpf::bindings::path };
+    let written = unsafe { aya_ebpf::helpers::gen::bpf_d_path(path, buf as *mut i8, size) };
+    return written;
+}
+
 fn try_file_open(ctx: LsmContext) -> Result<i32, i32> {
-    info!(&ctx, "lsm hook file_open called");
+    // Fetch the file struct being opened
+    let file: *const vmlinux::file = unsafe { ctx.arg(0) };
+
+    let mut buf: [u8; 256] = [0; 256];
+
+    let written = get_path_from_file(file, &buf as *const u8, 256);
+    let written = written as usize;
+
+    if written >= 256 {
+        return Ok(0);
+    }
+
+    let filter = "/home/menixator/test/".as_bytes();
+
+    match buf.get(0..21) {
+        Some(buf) if buf != filter => return Ok(0),
+        _ => {}
+    }
+
+    let path_as_str = unsafe { core::str::from_utf8_unchecked(&buf[0..written]) };
+
+    info!(&ctx, "lsm/file_open called for {}", path_as_str);
+
     Ok(0)
 }
 
