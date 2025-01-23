026890a6c4bcdfd9c13ba2d5dcc742fe66aa2f10
menixator <3684951+menixator@users.noreply.github.com>
2025-01-23 08:41:48 +0500

    feat: add all the fields of the event

--- migrations/20250122115135_initial_migration.up.sql
+++ migrations/20250122115135_initial_migration.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE events (
-    event VARCHAR(40) NOT NULL,               
-    path VARCHAR(4096) NOT NULL,      
-    path_secondary VARCHAR(4096), 
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    timestamp TIMESTAMPTZ NOT NULL
+    timestamp TIMESTAMPTZ NOT NULL,
+    username VARCHAR(256) NOT NULL,
+    groupname VARCHAR(256) NOT NULL,
+    event VARCHAR(40) NOT NULL,        
+    path VARCHAR(4096) NOT NULL,
+    path_secondary VARCHAR(4096) DEFAULT NULL,
 );
 
 CREATE INDEX idx_events_timestamp ON events(timestamp DESC);
