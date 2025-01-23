76b54f64e21c32c565eb78b5adf5a729508d343e
menixator <3684951+menixator@users.noreply.github.com>
2025-01-23 08:35:52 +0500

    feat: don't default timestamp and create desc indx on it alone

--- migrations/20250122115135_initial_migration.down.sql
+++ migrations/20250122115135_initial_migration.down.sql
@@ -1,5 +1,5 @@
 -- Drop the index first
-DROP INDEX IF EXISTS idx_events_id_timestamp;
+DROP INDEX IF EXISTS idx_events_timestamp;
 
 -- Drop the table
 DROP TABLE IF EXISTS events CASCADE;
--- migrations/20250122115135_initial_migration.up.sql
+++ migrations/20250122115135_initial_migration.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE events (
     path VARCHAR(4096) NOT NULL,      
     path_secondary VARCHAR(4096), 
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    timestamp TIMESTAMPTZ NOT NULL
 );
 
-CREATE INDEX idx_events_id_timestamp ON events(id, timestamp);
+CREATE INDEX idx_events_timestamp ON events(timestamp DESC);
