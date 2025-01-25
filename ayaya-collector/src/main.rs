635c87d86e3fcefbd73661fd114c03536b3c8a70
menixator <3684951+menixator@users.noreply.github.com>
2025-01-25 11:00:27 +0500

    feat: start building out the grpc server

--- ayaya-collector/src/main.rs
+++ ayaya-collector/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod ayaya_collection {
+    tonic::include_proto!("ayaya_collection");
+}
+
+use ayaya_collection::{
+    ayaya_trace_collection_server::{AyayaTraceCollection, AyayaTraceCollectionServer},
+    CollectRequest, ColletionReply, Trace,
+};
+
+#[derive(Debug, Default)]
+pub struct TraceCollector {}
+
+#[tonic::async_trait]
+impl AyayaTraceCollection for TraceCollector {
+    async fn collect(
+        &self,
+        request: tonic::Request<CollectRequest>,
+    ) -> std::result::Result<tonic::Response<ColletionReply>, tonic::Status> {
+        unimplemented!()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let service = TraceCollector::default();
+
+    Server::builder()
+        .add_service(AyayaTraceCollectionServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
